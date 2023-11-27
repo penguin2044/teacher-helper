853ade3fe0ad606c1fdfdcc4ddf3d78ef38e5224
penguin2044 <glasstree25@gmail.com>
2023-11-27 09:45:14 -0600

    32323

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let studentId = 0;
 const allStudent = {
     grades: [],
     names: [],
+    ben:0,
+    bob:100,
     getData: function () {
         let grade = document.getElementById('grade').value;
         let sName = document.getElementById('full-name').value;
@@ -22,6 +24,7 @@ const allStudent = {
         } else {
             alert('Enter the name or grade');
         }
+
     },
 
     average: function () {
@@ -30,7 +33,31 @@ const allStudent = {
         for (let i = 0; this.grades.length > i; i++) {
             sum += parseInt(this.grades[i]);
         }
-        document.getElementById('answer').innerText = `${(sum / this.grades.length).toFixed(2)}%`;        
+        document.getElementById('answer').innerText = `The grade average is ${(sum / this.grades.length).toFixed(2)}%`; 
+        this.low();
+        this.high(); 
+        document.getElementById('low').innerText = `The lowset grade is ${this.bob}%`; 
+        document.getElementById('high').innerText = `The highest grade is ${this.ben}%`;    
+ 
+
+    },
+    low: function(){
+        this.bob = 100;
+        for(let i=0;i < this.grades.length; i++){
+                if(this.grades[i] < this.bob)
+                this.bob = this.grades[i];
+            console.log(this.grades.length);
+        }
+        
+
+    },
+    high: function() {
+        
+        for(let i = 0; i < this.grades.length; i++){
+            if(this.grades[i] > this.ben)
+                this.ben = this.grades[i];
+        }
+        
     },
 
     pushInTable: function () {
@@ -39,17 +66,20 @@ const allStudent = {
         divRow.setAttribute('id', `student-${studentId}`);
 
         let divName = document.createElement('div');
-        divName.classList.add('name');
-        divName.innerText = allStudent.names[studentId];
+        divName.classList.add(  'name');
+        divName.innerText = `name: ${allStudent.names[studentId]}`;
+
 
         let divGrade = document.createElement('div');
         divGrade.classList.add('student-grade');
-        divGrade.innerText = allStudent.grades[studentId];
+        divGrade.innerText = `grade: ${allStudent.grades[studentId]}%`;
+
 
         // Button for deletion
         let buttonDel = document.createElement('button');
         buttonDel.setAttribute('onclick', `allStudent.deleteRow('student-${studentId}', ${studentId})`);
         buttonDel.innerText = 'Delete';
+
         
         studentsTable.appendChild(divRow);
 
@@ -74,6 +104,7 @@ const allStudent = {
         console.log(this.grades);
         studentId--;
         this.average();
+        
     }
 };
 
